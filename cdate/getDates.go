2f559d7c33aa9599c8989bb1d0804804d037d635
vivekyad4v <vivekyad4v@gmail.com>
2021-03-10 16:03:36 +0530

    Alowing vars export

--- cdate/getDates.go
+++ cdate/getDates.go
@@ -11,18 +11,18 @@ var (
 
 //GetDate ...
 type GetDate struct {
-	year  string
-	month string
-	day   string
-	yest  string
-	tms   int64
-	yms   int64
+	Year  string
+	Month string
+	Day   string
+	Yest  string
+	Tms   int64
+	Yms   int64
 }
 
-//GetDates ...
-type GetDates interface {
-	ymdyesterday()
-}
+// //GetDates ...
+// type GetDates interface {
+// 	Ymdyesterday()
+// }
 
 //Ymdyesterday ...
 func (getDate GetDate) Ymdyesterday() (getDateReturn GetDate) {
@@ -32,7 +32,7 @@ func (getDate GetDate) Ymdyesterday() (getDateReturn GetDate) {
 	ymsd := time.Date(y, m, d, 0, 0, 0, 0, time.UTC).UnixNano() / 1000000
 	tmsd := time.Date(y, m, s, 0, 0, 0, 0, time.UTC).UnixNano() / 1000000
 
-	getDateReturn.year, getDateReturn.month, getDateReturn.day, getDateReturn.yest, getDateReturn.tms, getDateReturn.yms = strconv.Itoa(y), strconv.Itoa(int(m)), strconv.Itoa(d), strconv.Itoa(s), ymsd, tmsd
+	getDateReturn.Year, getDateReturn.Month, getDateReturn.Day, getDateReturn.Yest, getDateReturn.Tms, getDateReturn.Yms = strconv.Itoa(y), strconv.Itoa(int(m)), strconv.Itoa(d), strconv.Itoa(s), ymsd, tmsd
 
 	return getDateReturn
 }
